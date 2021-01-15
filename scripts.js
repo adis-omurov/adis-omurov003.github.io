226f9d8eeb0dd8334a6a6bea1f9dd785c2f37bdc
adis omurov <adisomurov003@gmail.com>
2021-01-15 15:39:37 +0600

    unit

---
+++ scripts.js
@@ -0,0 +1,44 @@
+let first = "Adis";
+let second = first;
+let third = second;
+let fourth = third;
+let fifth = fourth;
+let capital = "London";
+let country = "GB";
+
+first = "Shrek";
+capital = "Bishkek";
+country = "KG";
+
+console.log(capital + " is the capital of " + country)
+
+let unit = "Distance";
+let subject = "meters";
+
+unit = "Bicycle";
+subject ="kilometers";
+unit = "hundred";
+subject = "thousand";
+
+console.log(unit + " is measured in " + subject);
+
+let title = "Dark Tower";
+let author = "Stiven King";
+
+title = "War and World"
+author = "Lev Tolstoi"
+
+console.log("My favourite book is " + title + " by " + author);
+
+console.log("My fovourite song is \"Yesterday\" by Beatles");
+
+console.log('I\'m the best');
+
+let artist = "Queen"
+artist = "Queen"
+
+console.log("I'm the fan of \""+ artist + "\"!");
+
+let film = "Hobbit";
+console.log(`My favourite movie is ` + `"` + film + `"`);
+console.log(`My favourite movie is "${film}"`);
